17cddcb6d42c4df924f624b04e9102cbc95acd98
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-26 11:21:09 -0600

    testing for extension started

---
+++ spec/features/hospital/show_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe 'Hospital show page' do 
+  before :each do
+    @grey = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
+    @seaside = Hospital.create!(name: 'Seaside Health & Wellness Center')
+    @swedish = Hospital.create!(name: 'Swedish Medical Center')
+    @meredith = @grey.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University')
+    @alex = @seaside.doctors.create!(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University')
+    @miranda = @swedish.doctors.create!(name: 'Miranda Bailey', specialty: 'General Surgery', university: 'Stanford University')
+    @derek = @grey.doctors.create!(name: 'Derek Shepherd', specialty: 'Attending Surgeon', university: 'University of Pennsylvania')
+    @katie = Patient.create!(name: 'Katie Bryce', age: 24)
+    @danny = Patient.create!(name: 'Denny Duquette', age: 39)
+    @rebecca = Patient.create!(name: 'Rebecca Pope', age: 32)
+    @zola = Patient.create!(name: 'Zola Shepherd', age: 2)
+    DoctorPatient.create!(doctor: @meredith, patient: @katie)
+    DoctorPatient.create!(doctor: @meredith, patient: @danny)
+    DoctorPatient.create!(doctor: @meredith, patient: @rebecca)
+    DoctorPatient.create!(doctor: @meredith, patient: @zola)
+    DoctorPatient.create!(doctor: @alex, patient: @katie)
+    DoctorPatient.create!(doctor: @alex, patient: @danny)
+    DoctorPatient.create!(doctor: @alex, patient: @rebecca)
+    DoctorPatient.create!(doctor: @miranda, patient: @katie)
+    DoctorPatient.create!(doctor: @miranda, patient: @danny)
+    DoctorPatient.create!(doctor: @derek, patient: @zola)
+  end
+
+  it '' do
+    # When I visit a hospital's show page
+    visit hospital_path(@grey)
+    # I see the hospital's name
+    expect(page).to have_content("Grey Sloan Memorial Hospital")
+    # And I see the names of all doctors that work at this hospital,
+    expect(page).to have_content("Meredith Grey")
+    expect(page).to have_content("Derek Shepherd")
+    expect(page).to_not have_content("Miranda Bailey")
+    # And next to each doctor I see the number of patients associated with the doctor,
+    within("#doctor-#{@meredith.id}") do
+      expect(page).to have_content("4 patients")
+      expect(page).to_not have_content("2 patients")
+    end
+    #  And I see the list of doctors is ordered from most number of patients to least number of patients
+    expect(@meredith).to appear_before(@alex)
+    expect(@alex).to appear_before(@miranda)
+    expect(@miranda).to appear_before(@derek)
+    #  (Doctor patient counts should be a single query)
+  end
+end
